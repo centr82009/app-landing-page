efbff3b274278f1539fc4e6745be524a52da2eb9
Роман Мазеев <centr82009@gmail.com>
2017-11-29 12:05:41 +0300

    Minor bug fixes

--- README.md
+++ README.md
@@ -1,10 +1,16 @@
-# Calculator PRO Website
+# App landing page (Example)
+
 ## Screenshot
+
 [![Screenshot](https://s7.postimg.org/5rrs84wwr/2017-11-24_4.16.52.png)](https://postimg.org/image/7wc597yjb/)
 
 ## Changhelog
-### 0.0.1
-* Made simple landing page
 
-## To do
-* Social icons
+### 0.0.2
+
+- Fix social icons
+- Minor bug fixes
+
+### 0.0.1
+
+- Made simple landing page
--- gulpfile.js
+++ gulpfile.js
@@ -18,7 +18,7 @@ var banner = ['/*!\n',
 
 // Compiles SCSS files from /scss into /css
 gulp.task('sass', function() {
-  return gulp.src('scss/new-age.scss')
+  return gulp.src('scss/sty.scss')
     .pipe(sass())
     .pipe(header(banner, {
       pkg: pkg
@@ -31,7 +31,7 @@ gulp.task('sass', function() {
 
 // Minify compiled CSS
 gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('css/new-age.css')
+  return gulp.src('css/style.css')
     .pipe(cleanCSS({
       compatibility: 'ie8'
     }))
@@ -46,7 +46,7 @@ gulp.task('minify-css', ['sass'], function() {
 
 // Minify custom JS
 gulp.task('minify-js', function() {
-  return gulp.src('js/new-age.js')
+  return gulp.src('js/style.js')
     .pipe(uglify())
     .pipe(header(banner, {
       pkg: pkg
